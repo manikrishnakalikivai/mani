71e0c08288fe8c974860178f2a3a69e693c5c8ae
push admin <manikrishnakalikivai@gmail.com>
2022-12-13 12:48:32 +0530

    java program

---
+++ A.java
@@ -0,0 +1,27 @@
+class Demo
+{
+    static int a=10;
+
+    static void display()
+
+    {
+
+     System.out.println("time decides everthing");
+
+    }
+}
+
+class StaticDemo
+{
+
+     public static void main (String [] args)
+
+     {
+
+     System.out.println("belive in yours");
+     Demo.display();
+     System.out.println(Demo.a);
+
+     }
+}
+
